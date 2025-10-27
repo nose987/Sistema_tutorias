f916528f17de732ccd5f6ef10036d33bc286938e
armando <amanjarrezh19@gmail.com>
2025-10-26 20:57:08 -0700

    Correción ortográfica

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,6 +2,6 @@
     <x-app-logo-icon class="size-5 fill-current text-white dark:text-black" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-bold">Sistema de turorias</span>
+    <span class="mb-0.5 truncate leading-tight font-bold">Sistema de tutorías</span>
     <span class="mb-0.5 truncate leading-tight font-regular">UTEsc</span>
 </div>
